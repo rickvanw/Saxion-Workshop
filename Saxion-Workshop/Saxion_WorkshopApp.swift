b04ca7642da7d72a149fb891762fd623ce31183c
Rick van Weersel <rick.vanweersel@justeattakeaway.com>
2023-12-07 13:11:56 +0100

    initial

---
+++ Saxion-Workshop/Saxion_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saxion_WorkshopApp.swift
+//  Saxion-Workshop
+//
+//  Created by Rick van Weersel on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Saxion_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
