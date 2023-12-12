c145d77c9f76dc55075901fe82024449e878a50a
Rick van Weersel <rick.vanweersel@justeattakeaway.com>
2023-12-12 15:41:16 +0100

    Add assets seperately

---
+++ Assets/MockData.swift
@@ -0,0 +1,35 @@
+//
+//  MockData.swift
+//  Saxion-Workshop
+//
+//  Created by Rick van Weersel on 07/12/2023.
+//
+
+public struct MockData {
+    static let pizzas = [
+        Pizza(name: "Margherita",
+              description: "With cheese",
+              longDescription: "In the heart of Italian culinary heritage, the Margherita pizza stands as a timeless masterpiece. Crafted on a foundation of thin, crispy-yet-chewy dough, it features a vibrant San Marzano tomato sauce drizzled with extra virgin olive oil. The star of the show is the locally-sourced fresh mozzarella, achieving golden perfection in a wood-fired oven. Topped with aromatic basil leaves, this pizza is a celebration of simplicity and quality. Each bite unveils a perfect harmony of sweet tomatoes, creamy cheese, and fragrant basil – a true culinary delight, elevating tradition to perfection.",
+              imageName: "margherita"),
+        Pizza(name: "Pepperoni", 
+              description: "With pepperoni",
+              longDescription: "Embark on a flavor-packed journey with the Pepperoni Pizza, a true delight for the senses. Crafted on a foundation of chewy yet thin dough, this pizza boasts a zesty tomato sauce with hints of garlic and oregano, creating the perfect canvas. Generously topped with crisped pepperoni slices, each bite promises a symphony of savory and spicy notes.",
+              imageName: "pepperoni"),
+        Pizza(name: "Four Cheese",
+              description: "With four cheeses",
+              longDescription: "Embark on a taste adventure with our Four Cheese Pizza, a sensory delight. Crafted on a base of chewy yet thin dough, it showcases a luscious blend of four premium cheeses – creamy mozzarella, sharp cheddar, tangy Parmesan, and indulgent Gorgonzola. The canvas? A zesty tomato sauce kissed with hints of garlic and oregano. Every bite promises a symphony of rich, melty goodness, elevating your pizza experience to a realm of pure indulgence.",
+              imageName: "fourcheese"),
+        Pizza(name: "Funghi",
+              description: "With mushrooms",
+              longDescription: "Embark on a delightful journey with our Pizz Funghi, a pizza that celebrates the earthy richness of mushrooms. Crafted on a foundation of chewy yet thin dough, this pizza features a canvas of zesty tomato sauce with subtle hints of garlic and oregano. Crowned with a medley of perfectly sautéed mushrooms, each bite promises a harmonious blend of savory umami flavors. It's a mushroom lover's dream, a simple yet sophisticated indulgence that elevates the pizza experience to new heights.",
+              imageName: "funghi"),
+        Pizza(name: "Quattro Stagioni",
+              description: "With diverse ingredients",
+              longDescription: "Embark on a culinary journey with our Quattro Stagioni pizza, an ode to the changing seasons. Crafted on a foundation of thin, crispy-yet-chewy dough, this masterpiece is divided into four sections, each representing a season's bounty. 1. Spring: Tender asparagus spears mingle with fresh cherry tomatoes, capturing the vibrant essence of spring. 2. Summer: Sun-kissed artichoke hearts and juicy olives evoke the warmth and richness of summer. 3. Autumn: Earthy sautéed mushrooms, representing the fall harvest, create a cozy and savory sensation. 4. Winter: A touch of ham adds a hearty, wintry flavor, completing the symphony. Unified by a base of zesty tomato sauce and drizzled with extra virgin olive oil, our Quattro Stagioni celebrates the beauty of each season in a harmonious blend of flavors. It's a culinary delight that transcends traditional boundaries, offering a slice of all four seasons in one delicious experience.",
+              imageName: "quattrostagioni"),
+        Pizza(name: "Vegan Margherita",
+              description: "With vegan cheese",
+              longDescription: "In the heart of Italian culinary heritage, the Vegan Margherita pizza stands as a timeless plant-based masterpiece. Crafted on a foundation of thin, crispy-yet-chewy vegan dough, it features a vibrant tomato sauce made from ripe San Marzano tomatoes and drizzled with extra virgin olive oil. The star of the show is locally-sourced vegan mozzarella, achieving a golden perfection in a wood-fired oven. Topped with aromatic basil leaves, this vegan pizza is a celebration of simplicity and quality, showcasing a perfect harmony of sweet tomatoes, creamy vegan cheese, and fragrant basil in every delightful bite – a true culinary sensation, elevating tradition to vegan perfection.",
+              imageName: "margherita")
+    ]
+}
