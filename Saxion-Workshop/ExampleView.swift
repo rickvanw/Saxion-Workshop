8e9bf4559a06fb4dfa8ebbcbfa43b12d3c2df1e8
Mariëlle van der Meulen <marielle.vandermeulen@justeattakeaway.com>
2023-12-12 13:22:13 +0100

    Added ExampleView

--- Saxion-Workshop/ExampleView.swift
+++ Saxion-Workshop/ExampleView.swift
@@ -2,7 +2,54 @@
 //  ExampleView.swift
 //  Saxion-Workshop
 //
-//  Created by Mariëlle van der Meulen on 12/12/2023.
+//  Created by Mariëlle van der Meulen on 06/12/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct ExampleView: View {
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        VStack {
+            Text("This is my orange heart")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.red)
+            Text("🧡")
+                .font(.system(size: 110))
+            Button("Love it!") {
+                dismiss()
+            }
+        }
+        Divider()
+
+        HStack {
+            Text("This is my orange heart")
+                .font(.title)
+                .fontWeight(.light)
+                .foregroundStyle(.teal)
+            Text("🧡")
+                .font(.system(size: 110))
+            Button("Love it!") {
+                dismiss()
+            }
+        }
+        Divider()
+        
+        ZStack {
+            Text("This is my nice Pizza app")
+                .font(.title)
+                .fontWeight(.ultraLight)
+            Text("🧡")
+                .font(.system(size: 110))
+            Button("Love it!") {
+                dismiss()
+            }
+        }
+    }
+}
+
+#Preview {
+    ExampleView()
+}
